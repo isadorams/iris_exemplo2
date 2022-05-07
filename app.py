a067c00570c4969cba2fe6b054ebe7c9e3600e3b
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-06 22:13:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
 
 st.write("# Classificação de Iris")
 st.write("## Exemplo com o comprimento e largura da pétala e sépala")
@@ -8,3 +12,31 @@ st.sidebar.slider("Comprimento da sépala", 4.0, 8.0, 5.8, 0.1)
 st.sidebar.slider("Largura da sépala", 2.0, 5.0, 3.6, 0.1)
 st.sidebar.slider("Comprimento da pétala", 0.9, 7, 3.8, 0.1)
 st.sidebar.slider("Largura da pétala", 0.8, 3, 1.4, 0.1)
+
+
+with open("objetos.pkl", "rb") as arquivo:
+  ss, dtc = pickle.load(arquivo)
+
+  estrutura = {'comp_sepala': comp_sepala, 'comp_petala': comp_petala}
+  df = pd.DataFrame(estrutura, index=[0])
+  
+  st.write("### Parâmetros de Entrada")
+  st.write(df)
+  
+  df = ss.transform(df)
+  st.write(df)
+  
+  predicao = dtc.predict(df)
+  st.write(f"A classe dessa flor é: **{predicao[0]}**")
+  #st.write("**A classe dessa flor é:**")
+  #st.write(predicao)
+  predicao = dtc.predict_proba(df)
+  predicao = pd.DataFrame(predicao)
+  predicao.rename({
+    0: "Iris-setosa",
+    1: "Iris-versicolor",
+    2: "Iris-virginica"
+  }, axis=1, inplace=True)
+  
+  st.write("Probabilidades")
+  st.write(predicao)
